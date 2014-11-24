c8517d9964a0c5fd0b8ed9b16b744b755d91f649
rap2hpoutre <rap2hpoutre@users.noreply.github.com>
2014-11-24 11:17:52 +0100

    Update LogViewerController.php

--- src/controllers/LogViewerController.php
+++ src/controllers/LogViewerController.php
@@ -9,7 +9,7 @@ class LogViewerController extends \BaseController
     public function index()
     {
         $logs = LaravelLogViewer::all();
-        View::addNamespace('laravel-log-viewer', __DIR__.'../../views');
+        View::addNamespace('laravel-log-viewer', __DIR__.'/../views');
         return View::make('laravel-log-viewer::log', ['logs' => $logs]);
     }
 
