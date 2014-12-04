35e9d82c616f9bdb010f621b1378fcc88d8f06a6
rap2hpoutre <rap2hpoutre@users.noreply.github.com>
2014-12-04 14:18:17 +0100

    Update LaravelLogViewer.php

--- src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewer.php
+++ src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewer.php
@@ -5,11 +5,21 @@ use Illuminate\Support\Facades\File;
 use Psr\Log\LogLevel;
 use ReflectionClass;
 
+/**
+ * Class LaravelLogViewer
+ * @package Rap2hpoutre\LaravelLogViewer
+ */
 class LaravelLogViewer
 {
 
+    /**
+     * @var current file
+     */
     private static $file;
 
+    /**
+     * @param $file
+     */
     public static function setFile($file)
     {
         if (File::exists(storage_path() . '/logs/' . $file)) {
@@ -17,6 +27,9 @@ class LaravelLogViewer
         }
     }
 
+    /**
+     * @return array
+     */
     public static function all()
     {
         $log = array();
@@ -32,8 +45,6 @@ class LaravelLogViewer
             $log_file = self::getFiles();
             $file = File::get($log_file[0]);
         }
-
-
 
         preg_match_all($pattern, $file, $headings);
 
@@ -80,7 +91,7 @@ class LaravelLogViewer
                             'date' => $current[1],
                             'text' => $current[2],
                             'in_file' => isset($current[3]) ? $current[3] : null,
-                            'stack' => $log_data[$i],
+                            'stack' => preg_replace("/^\n*/", '', $log_data[$i])
                         );
                     }
                 }
@@ -91,6 +102,10 @@ class LaravelLogViewer
         return $log;
     }
 
+    /**
+     * @param bool $basename
+     * @return array
+     */
     public static function getFiles($basename = false)
     {
         $files = glob(storage_path() . '/logs/*');
