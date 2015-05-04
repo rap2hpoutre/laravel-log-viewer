882240235a23e7a4c2c315c9a0624572301d71a7
Robin Houtevelts <robin@houtevelts.com>
2015-05-04 16:24:09 +0200

    Add ajax live update
    As requested in https://github.com/rap2hpoutre/laravel-log-viewer/issues/26

--- src/controllers/LogViewerController.php
+++ src/controllers/LogViewerController.php
@@ -14,6 +14,23 @@ class LogViewerController extends \Illuminate\Routing\Controller
 
         $logs = LaravelLogViewer::all();
 
+        if (\Input::get('n')) {
+          $offset = \Input::get('n');
+          $logs = array_slice($logs, 0, count($logs) - $offset);
+        }
+
+        if (\Request::ajax()) {
+          $tmpLogs = $logs;
+          $logs = [];
+
+          // Add log['key'] to the array.
+          foreach($tmpLogs as $key => $log) {
+            $log['key'] = $key;
+            $logs[] = $log;
+          }
+          return $logs;
+        }
+
         return View::make('laravel-log-viewer::log', [
             'logs' => $logs,
             'files' => LaravelLogViewer::getFiles(true),
