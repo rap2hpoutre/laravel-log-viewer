5649c0bc37d8941a2408ed99d0f6f87a83f3eead
Iftekhar Rifat <rifat662@gmail.com>
2016-12-15 12:06:16 +0600

    Remove facade dependency

--- src/controllers/LogViewerController.php
+++ src/controllers/LogViewerController.php
@@ -7,32 +7,32 @@ if (class_exists("\\Illuminate\\Routing\\Controller")) {
     class BaseController extends \Laravel\Lumen\Routing\Controller {}
 }
 
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\View;
-use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\Request;
-use Illuminate\Support\Facades\Response;
-
 class LogViewerController extends BaseController
 {
+    protected $request;
+
+    public function __construct ()
+    {
+        $this->request = app('request');
+    }
 
     public function index()
     {
 
-        if (Request::input('l')) {
-            LaravelLogViewer::setFile(base64_decode(Request::input('l')));
+        if ($this->request->input('l')) {
+            LaravelLogViewer::setFile(base64_decode($this->request->input('l')));
         }
 
-        if (Request::input('dl')) {
-            return Response::download(LaravelLogViewer::pathToLogFile(base64_decode(Request::input('dl'))));
-        } elseif (Request::has('del')) {
-            File::delete(LaravelLogViewer::pathToLogFile(base64_decode(Request::input('del'))));
-            return $this->redirect(Request::url());
+        if ($this->request->input('dl')) {
+            return response()->download(LaravelLogViewer::pathToLogFile(base64_decode($this->request->input('dl'))));
+        } elseif ($this->request->has('del')) {
+            app('files')->delete(LaravelLogViewer::pathToLogFile(base64_decode($this->request->input('del'))));
+            return $this->redirect($this->request->url());
         }
 
         $logs = LaravelLogViewer::all();
 
-        return View::make('laravel-log-viewer::log', [
+        return app('view')->make('laravel-log-viewer::log', [
             'logs' => $logs,
             'files' => LaravelLogViewer::getFiles(true),
             'current_file' => LaravelLogViewer::getFileName()
@@ -45,6 +45,6 @@ class LogViewerController extends BaseController
             return redirect($to);
         }
 
-        return Redirect::to($to);
+        return app('redirect')->to($to);
     }
 }
