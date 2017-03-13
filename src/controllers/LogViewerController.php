f95d4cf5d00c0d2fcaa4b7c0528fca53aadfd4e1
raph <rap2hpoutre@users.noreply.github.com>
2017-03-13 13:37:28 +0100

    Update LogViewerController.php

--- src/controllers/LogViewerController.php
+++ src/controllers/LogViewerController.php
@@ -28,7 +28,7 @@ class LogViewerController extends BaseController
         } elseif ($this->request->has('del')) {
             app('files')->delete(LaravelLogViewer::pathToLogFile(base64_decode($this->request->input('del'))));
             return $this->redirect($this->request->url());
-        } elseif ($this->request->has('delall')){
+        } elseif ($this->request->has('delall')) {
             foreach(LaravelLogViewer::getFiles(true) as $file){
                 app('files')->delete(LaravelLogViewer::pathToLogFile($file));
             }
