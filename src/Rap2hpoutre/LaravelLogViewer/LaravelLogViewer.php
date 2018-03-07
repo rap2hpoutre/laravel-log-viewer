6b4fd369ed0a92d314d36ebb6622c998796bd79f
Seçkin KILINÇ <xembill@gmail.com>
2018-03-07 17:13:26 +0300

    Xembill patch 1 (#124)
    For supervisor log "failed"

--- src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewer.php
+++ src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewer.php
@@ -24,6 +24,7 @@ class LaravelLogViewer
         'alert' => 'danger',
         'emergency' => 'danger',
         'processed' => 'info',
+        'failed' => 'warning',
     ];
 
     private static $levels_imgs = [
@@ -35,7 +36,8 @@ class LaravelLogViewer
         'critical' => 'exclamation-triangle',
         'alert' => 'exclamation-triangle',
         'emergency' => 'exclamation-triangle',
-        'processed' => 'info-circle'
+        'processed' => 'info-circle',
+        'failed' => 'warning'
     ];
 
     /**
@@ -51,7 +53,8 @@ class LaravelLogViewer
         'notice',
         'info',
         'debug',
-        'processed'
+        'processed',
+        'failed'
     ];
 
     const MAX_FILE_SIZE = 52428800; // Why? Uh... Sorry
