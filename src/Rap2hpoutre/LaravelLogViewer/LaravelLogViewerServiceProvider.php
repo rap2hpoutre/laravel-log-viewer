3ee40c9e4bf357de83f96d7b60672e17b3aa5e4a
rap2h <raphaelht@gmail.com>
2018-03-07 15:18:25 +0100

    tabs to space

--- src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewerServiceProvider.php
+++ src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewerServiceProvider.php
@@ -4,57 +4,57 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelLogViewerServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		if (method_exists($this, 'package')) {
-			$this->package('rap2hpoutre/laravel-log-viewer', 'laravel-log-viewer', __DIR__ . '/../../');
-		}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if (method_exists($this, 'package')) {
+            $this->package('rap2hpoutre/laravel-log-viewer', 'laravel-log-viewer', __DIR__ . '/../../');
+        }
 
-		if (method_exists($this, 'loadViewsFrom')) {
-			$this->loadViewsFrom(__DIR__.'/../../views', 'laravel-log-viewer');
-		}
-		
-		if (method_exists($this, 'publishes')) {
-		    $this->publishes([
-		       	__DIR__.'/../../views' => base_path('/resources/views/vendor/laravel-log-viewer'),
-		    ], 'views');
-		    $this->publishes([
-		        __DIR__.'/../../config/logviewer.php' => config_path('logviewer.php'),
-		    ]);
+        if (method_exists($this, 'loadViewsFrom')) {
+            $this->loadViewsFrom(__DIR__.'/../../views', 'laravel-log-viewer');
+        }
+        
+        if (method_exists($this, 'publishes')) {
+            $this->publishes([
+                   __DIR__.'/../../views' => base_path('/resources/views/vendor/laravel-log-viewer'),
+            ], 'views');
+            $this->publishes([
+                __DIR__.'/../../config/logviewer.php' => config_path('logviewer.php'),
+            ]);
 
-		}
-	}
+        }
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
