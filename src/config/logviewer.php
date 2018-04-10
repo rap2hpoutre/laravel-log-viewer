b1d114cdde04fd7eb2a49cac732aa251f28bbc64
MeijdenMedia <joost@meijdenmedia.nl>
2018-04-10 10:55:04 +0200

    Added description in log

--- src/config/logviewer.php
+++ src/config/logviewer.php
@@ -2,5 +2,14 @@
 
 return [
     'pattern'      => env('LOGVIEWER_PATTERN', '*.log'),
+    
+    /*
+    |--------------------------------------------------------------------------
+    | Clickable table rows
+    |--------------------------------------------------------------------------
+    |
+    | When this value is set to true the whole <tr> is clickable and will open/close the stacktrace.
+    |
+    */
     'tr_clickable' => env('LOGVIEWER_TR_CLICKABLE', true),
 ];
