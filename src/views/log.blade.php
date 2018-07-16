41b91a1058fa779982a9a21c6a29537699e50d82
rap2h <raphaelht@gmail.com>
2018-07-16 11:18:23 +0200

    refactor

--- src/views/log.blade.php
+++ src/views/log.blade.php
@@ -115,21 +115,29 @@
           @foreach($logs as $key => $log)
             <tr data-display="stack{{{$key}}}">
               @if ($standardFormat)
-                <td class="text-{{{$log['level_class']}}}"><span class="fa fa-{{{$log['level_img']}}}"
-                                                                aria-hidden="true"></span> &nbsp;{{$log['level']}}</td>
+                <td class="text-{{{$log['level_class']}}}">
+                  <span class="fa fa-{{{$log['level_img']}}}" aria-hidden="true"></span>&nbsp;&nbsp;{{$log['level']}}
+                </td>
                 <td class="text">{{$log['context']}}</td>
               @endif
               <td class="date">{{{$log['date']}}}</td>
               <td class="text">
-                @if ($log['stack']) <button type="button" class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2"
-                                       data-display="stack{{{$key}}}"><span
-                      class="fa fa-search"></span></button>@endif
+                @if ($log['stack'])
+                  <button type="button"
+                          class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2"
+                          data-display="stack{{{$key}}}">
+                    <span class="fa fa-search"></span>
+                  </button>
+                @endif
                 {{{$log['text']}}}
-                @if (isset($log['in_file'])) <br/>{{{$log['in_file']}}}@endif
+                @if (isset($log['in_file']))
+                  <br/>{{{$log['in_file']}}}
+                @endif
                 @if ($log['stack'])
                   <div class="stack" id="stack{{{$key}}}"
                        style="display: none; white-space: pre-wrap;">{{{ trim($log['stack']) }}}
-                  </div>@endif
+                  </div>
+                @endif
               </td>
             </tr>
           @endforeach
