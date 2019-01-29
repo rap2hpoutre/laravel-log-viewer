a77747b9b404f727b90fcbc26b92ec46160209af
Vania Kucher <dev.kucher@gmail.com>
2019-01-29 16:53:06 +0200

    Hide from search robots

--- src/views/log.blade.php
+++ src/views/log.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="robots" content="noindex, nofollow">
   <title>Laravel log viewer</title>
   <link rel="stylesheet"
         href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
