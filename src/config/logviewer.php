b7594662cc3ffcacc103b0c2c70802040b5dfc38
Clément Baconnier <clement.baconnier@gmail.com>
2019-08-05 10:57:12 +0200

    Fix typo

--- src/config/logviewer.php
+++ src/config/logviewer.php
@@ -9,7 +9,7 @@ return [
     | The env key for pattern and storage path with a default value
     |
     */
-    'max_file_size' => 52428800, // size in Mb
+    'max_file_size' => 52428800, // size in Byte
     'pattern'       => env('LOGVIEWER_PATTERN', '*.log'),
     'storage_path'  => env('LOGVIEWER_STORAGE_PATH', storage_path('logs')),
 ];
