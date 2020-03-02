e59cedf2c738af7b64236c407803fe4d8fd474d5
Gerardo Ibarra <gibarra@upgradeit.com.ar>
2020-03-02 00:57:59 -0300

    Lumen fix

--- src/Rap2hpoutre/LaravelLogViewer/Pattern.php
+++ src/Rap2hpoutre/LaravelLogViewer/Pattern.php
@@ -10,6 +10,10 @@ namespace Rap2hpoutre\LaravelLogViewer;
 
 class Pattern
 {
+    /**
+     * @var \Illuminate\Foundation\Application | \Laravel\Lumen\Application
+     */
+    private $app;
 
     /**
      * @var array
@@ -32,6 +36,38 @@ class Pattern
         'files' => '/\{.*?\,.*?\}/i',
     ];
 
+
+    /**
+     * @var array
+     */
+    private $patternsLumen = [
+        'logs' => '/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}([\+-]\d{4})?\].*/',
+        'current_log' => [
+            '/^\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}([\+-]\d{4})?)\](?:.*?(\w+)\.|.*?)',
+            ': ([^ ]+):(\d)? *(.*?) in (.*?):([0-9]+)$/i',
+            ': (.+) ((\{.+\}))? $/i',
+        ],
+        'current_log_string' => '/^([^ ]+): *(.*?) in (.*?):([0-9]+)$/',
+        'stack_init_section' => '/^\nStack trace:\n/',
+        'stack' => [
+            '/^(.+)(->|::)([^\(]+)\((.*)\)$/',
+            '/^([^\(]+)\((.*)\)$/',
+            '/^(.+)\(([0-9]+)\): (.+)$/',
+        ],
+        'stack_startWith' => '/^ ?\#? ?[0-9]+ ?/',
+        'files' => '/\{.*?\,.*?\}/i',
+    ];
+
+    /**
+     * Pattern constructor.
+     */
+    public function __construct()
+    {
+        if (function_exists('app')) {
+            $this->app = app();
+        }
+    }
+
     /**
      * @return array
      */
@@ -48,10 +84,36 @@ class Pattern
     public function getPattern($pattern, $position = null)
     {
         $patternVersion = $this->patterns;
+        if ($this->isLumen()) {
+            $patternVersion = $this->patternsLumen;
+        }
         if ($position !== null) {
             return $patternVersion[$pattern][$position];
         }
         return $patternVersion[$pattern];
 
     }
+
+    /**
+     * @return bool
+     */
+    public function isLaravel()
+    {
+        if (is_a($this->app, '\Illuminate\Foundation\Application')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLumen()
+    {
+        if (is_a($this->app, '\Laravel\Lumen\Application')) {
+            return true;
+        }
+        return false;
+    }
+
 }
