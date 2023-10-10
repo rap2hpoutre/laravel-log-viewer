2ade35e2eb4b19acfc1e150ac9c001645a61af13
Iman Ghafoori <imanghafoori1@gmail.com>
2023-10-10 13:25:21 +0330

    Reformat the service provider

--- src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewerServiceProvider.php
+++ src/Rap2hpoutre/LaravelLogViewer/LaravelLogViewerServiceProvider.php
@@ -1,8 +1,11 @@
-<?php namespace Rap2hpoutre\LaravelLogViewer;
+<?php
+
+namespace Rap2hpoutre\LaravelLogViewer;
 
 use Illuminate\Support\ServiceProvider;
 
-class LaravelLogViewerServiceProvider extends ServiceProvider {
+class LaravelLogViewerServiceProvider extends ServiceProvider
+{
     /**
      * Bootstrap the application events.
      *
@@ -11,21 +14,20 @@ class LaravelLogViewerServiceProvider extends ServiceProvider {
     public function boot()
     {
         if (method_exists($this, 'package')) {
-            $this->package('rap2hpoutre/laravel-log-viewer', 'laravel-log-viewer', __DIR__ . '/../../');
+            $this->package('rap2hpoutre/laravel-log-viewer', 'laravel-log-viewer', __DIR__.'/../../');
         }
 
         if (method_exists($this, 'loadViewsFrom')) {
             $this->loadViewsFrom(__DIR__.'/../../views', 'laravel-log-viewer');
         }
-        
+
         if (method_exists($this, 'publishes')) {
             $this->publishes([
-                   __DIR__.'/../../views' => base_path('/resources/views/vendor/laravel-log-viewer'),
+                __DIR__.'/../../views' => base_path('/resources/views/vendor/laravel-log-viewer'),
             ], 'views');
             $this->publishes([
                 __DIR__.'/../../config/logviewer.php' => $this->config_path('logviewer.php'),
             ]);
-
         }
     }
 
@@ -42,12 +44,12 @@ class LaravelLogViewerServiceProvider extends ServiceProvider {
     /**
      * Get the configuration path.
      *
-     * @param  string $path
+     * @param  string  $path
      * @return string
      */
     private function config_path($path = '')
     {
-        return function_exists('config_path') ? config_path($path) : app()->basePath() . DIRECTORY_SEPARATOR . 'config' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
+        return function_exists('config_path') ? config_path($path) : app()->basePath().DIRECTORY_SEPARATOR.'config'.($path ? DIRECTORY_SEPARATOR.$path : $path);
     }
 
 }
